310ae7a11a8765872f29b9a159a7b2af8fd26627
Keyla <keylato96@gmail.com>
2021-02-17 09:47:04 -0700

    Se crea array bidimensional

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace array_bidimensional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,] numeros = new int [3,3] {{1,3,5}, {2,4,6}, {3, 5,9}};
+
+            Console.Write(numeros[0,0]);
+            Console.Write(numeros[0,1]);
+            Console.Write(numeros[0,2 + \n ]);
+            Console.Write(numeros[1,0]);
+            Console.Write(numeros[1,1]);
+            Console.Write(numeros[1,2 + \n ]);
+            Console.Write(numeros[2,0]);
+            Console.Write(numeros[2,1]);
+            Console.Write(numeros[2,2+ \n ]);
+
+            int cadro1= (numeros[0,0] * numeros[1,1] * numeros[2,2]);
+            int cuadro2= (numeros [0,1] * numeros [1,2] * numeros [2,0]);
+            int cuadro3= (numeros [0,2] * numeros [1,0]* numeros [2,1]);
+            int cuadro4= (numeros [0,2] * numeros [1,1] * numeros [2,0]);
+            int cuadro5 = (numeros [0,1] * numeros [1,2] * numeros [2,1]);
+            int cuandro6 =(numeros [0,2] * numeros [1,1] * numeros [2,0]);
+
+            int operacion= cuadro1 + cuadro2 + cuadro3 -cuadro4 -cuadro5 -cuandro6;
+
+            Console.Writeline(operacion);
+
+
+
+
+        }
+
+
+    }
+}
